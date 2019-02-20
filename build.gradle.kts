a3091c4fab23713248b4d76fbc07ed7a402f2d38
David LJ <mail@davidlj95.com>
2019-02-20 13:12:40 +0100

    Building Kotlin JVM with libraries tutorial

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.jvm.tasks.Jar
+
+plugins {
+    `build-scan`
+    `maven-publish`
+    kotlin("jvm") version "1.3.21" 
+    id("org.jetbrains.dokka") version "0.9.17"
+}
+
+group = "com.davidlj95"
+version = "0.0.1"
+
+repositories {
+    jcenter() 
+}
+
+dependencies {
+    implementation(kotlin("stdlib")) 
+    testImplementation("junit:junit:4.12")
+}
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+}
+
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(tasks.dokka)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            artifact(dokkaJar)
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repository")
+        }
+    }
+}
---
+++ src/test/kotlin/com/davidlj95/MyLibraryTest.kt
@@ -0,0 +1,11 @@
+package com.davidlj95
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MyLibraryTest {
+    @Test fun testMyLanguage() {
+        assertEquals("Kotlin", MyLibrary().kotlinLanguage().name)
+        assertEquals(10, MyLibrary().kotlinLanguage().hotness)
+    }
+}
